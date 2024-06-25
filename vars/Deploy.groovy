81c43abce3a89c340db6a960bec28cb88032b12f
norhanhatem0187 <norhan.hatem0187@gmail.com>
2024-06-26 00:50:03 +0300

    Update Deploy.groovy

--- vars/Deploy.groovy
+++ vars/Deploy.groovy
@@ -1,6 +1,6 @@
 #!usr/bin/env groovy
 
-def call(String OpenShiftCredentialsID, String openshiftClusterurl, String openshiftProject, String imageName) {
+def call(String imageName) {
 
     // Update deployment.yaml with new Docker Hub image
     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"
