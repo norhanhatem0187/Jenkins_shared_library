f04f0cee4b2b9278470d8b526ede7b52df745412
norhanhatem0187 <norhan.hatem0187@gmail.com>
2024-06-26 02:47:16 +0300

    Update Deploy.groovy

--- vars/Deploy.groovy
+++ vars/Deploy.groovy
@@ -7,7 +7,7 @@ def call(String imageName) {
 
     // login to OpenShift Cluster via cluster url & service account token
     
-            sh "kubectl apply -f ."
+            sh "kubectl apply -f . --validate=false"
    
 
 }
